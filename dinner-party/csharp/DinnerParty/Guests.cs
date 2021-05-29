7917637a1bb93151aebad3db5da6f6b4617ee5c5
Chadwell Clark <wcclark23@comcast.net>
2021-05-28 20:53:35 -0500

    Created starter files for last two exercises

---
+++ dinner-party/csharp/DinnerParty/Guests.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+namespace DinnerParty
+{
+    public class Guests
+    {
+        Dictionary<string, string> guests = new Dictionary<string, string>()
+        {
+            {}
+        };
+    }
+}
+// function getGuests() {
+//   return [
+//     {
+//       name: "Marilyn Monroe",
+//       occupation: "entertainer",
+//       bio: "(1926 - 1962) American actress, singer, model"
+//     },
+//     {
+//       name: "Abraham Lincoln",
+//       occupation: "politician",
+//       bio: "(1809 - 1865) US President during American civil war"
+//     },
+//     {
+//       name: "Martin Luther King",
+//       occupation: "activist",
+//       bio: "(1929 - 1968)  American civil rights campaigner"
+//     },
+//     {
+//       name: "Rosa Parks",
+//       occupation: "activist",
+//       bio: "(1913 - 2005)  American civil rights activist"
+//     },
+//     {
+//       name: "Peter Sellers",
+//       occupation: "entertainer",
+//       bio: "(1925 - 1980) British actor and comedian"
+//     },
+//     {
+//       name: "Alan Turing",
+//       occupation: "computer scientist",
+//       bio: "(1912 - 1954) - British computing pioneer, Turing machine, algorithms, cryptology, computer architecture, saved the world"
+//     },
+//     {
+//       name: "Admiral Grace Hopper",
+//       occupation: "computer scientist",
+//       bio: "(1906–1992) - developed early compilers: FLOW-Matic, COBOL; worked on UNIVAC; gave speeches on computer history, where she gave out nano-seconds"
+//     },
+//     {
+//       name: "Indira Gandhi",
+//       occupation: "politician",
+//       bio: "(1917 - 1984) Prime Minister of India 1966 - 1977",
+//     }
